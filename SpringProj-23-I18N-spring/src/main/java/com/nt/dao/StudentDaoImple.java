d79a1166a8b5c4c4304319369bf89f60fc622a1c
Deepak <54569343+Deepak5035@users.noreply.github.com>
2020-07-06 08:42:12 +0530

    commit 1

---
+++ SpringProj-23-I18N-spring/src/main/java/com/nt/dao/StudentDaoImple.java
@@ -0,0 +1,41 @@
+package com.nt.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.sql.DataSource;
+
+import com.nt.bo.StudentBo;
+
+public class StudentDaoImple implements StudentDao {
+	private static final String STUDENT_INSERT="INSERT INTO STUDENT_TABLE  VALUES(?,?,?,?,?)";
+	private DataSource ds=null;
+	
+	public StudentDaoImple(DataSource ds) {
+		this.ds = ds;
+	}
+	@Override
+	public int insert(StudentBo bo) throws Exception {
+	Connection con=null;
+	PreparedStatement ps=null;
+	
+	int count=0;
+	//get connection object from conn pool
+	con=ds.getConnection();
+	//create prepare statement object having precompile Sql query
+	ps=con.prepareStatement(STUDENT_INSERT);
+    ps.setString(1, bo.getsName());
+    ps.setString(2, bo.getsAdd());
+    ps.setDouble(3, bo.getTotal());
+    ps.setDouble(4, bo.getAvg());
+    ps.setString(5, bo.getResult());
+    //execute PreCompile sql query
+    count=ps.executeUpdate();
+    //close jdbc connections
+    ps.close();
+    con.close();
+	return count;
+	}
+
+}
---
+++ SpringProj-23-I18N-spring/src/main/java/com/nt/dto/StudentDto.java
@@ -0,0 +1,39 @@
+package com.nt.dto;
+
+public class StudentDto {
+     String sName=null;
+     String sAdd=null;
+     int m1=0;
+     int m2=0;
+     int m3=0;
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	public String getsAdd() {
+		return sAdd;
+	}
+	public void setsAdd(String sAdd) {
+		this.sAdd = sAdd;
+	}
+	public int getM1() {
+		return m1;
+	}
+	public void setM1(int m1) {
+		this.m1 = m1;
+	}
+	public int getM2() {
+		return m2;
+	}
+	public void setM2(int m2) {
+		this.m2 = m2;
+	}
+	public int getM3() {
+		return m3;
+	}
+	public void setM3(int m3) {
+		this.m3 = m3;
+	}
+}
