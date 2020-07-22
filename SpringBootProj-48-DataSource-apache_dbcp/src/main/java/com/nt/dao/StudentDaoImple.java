78e017cb8e9d5ae8a91f2c814199135f86ac71d0
Deepak <54569343+Deepak5035@users.noreply.github.com>
2020-07-22 08:06:13 +0530

    commit 1

---
+++ SpringBootProj-48-DataSource-apache_dbcp/src/main/java/com/nt/dao/StudentDaoImple.java
@@ -0,0 +1,49 @@
+package com.nt.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.nt.bo.StudentBo;
+
+@Repository("stuDao")
+public class StudentDaoImple implements StudentDao {
+	private static final String STUDENT_INSERT="INSERT INTO STUDENT_TABLE  VALUES(?,?,?,?,?)";
+	
+	@Autowired
+	private DataSource ds=null;
+	
+	public StudentDaoImple(DataSource ds) {
+		this.ds = ds;
+	}
+	
+	@Override
+	public int insert(StudentBo bo) throws Exception {
+	Connection con=null;
+	PreparedStatement ps=null;
+	
+	int count=0;
+	//get connection object from conn pool
+	con=ds.getConnection();
+	//create prepare statement object having precompile Sql query
+	ps=con.prepareStatement(STUDENT_INSERT);
+    ps.setString(1, bo.getsName());
+    ps.setString(2, bo.getsAdd());
+    ps.setDouble(3, bo.getTotal());
+    ps.setDouble(4, bo.getAvg());
+    ps.setString(5, bo.getResult());
+    //execute PreCompile sql query
+    count=ps.executeUpdate();
+    //close jdbc connections
+    ps.close();
+    con.close();
+	return count;
+	}
+
+}
